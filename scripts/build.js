2bd22dea63d0f437c4b2783762ce1804bf484939
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2023-01-13 13:33:47 +0100

    feat: dna-storybook-vite

--- scripts/build.js
+++ scripts/build.js
@@ -1,5 +1,7 @@
 import esbuild from 'esbuild';
 
+// dna-storybook-renderer
+
 esbuild.build({
     entryPoints: [
         './packages/dna-storybook-renderer/src/index.ts',
@@ -27,5 +29,36 @@ esbuild.build({
     platform: 'browser',
     format: 'esm',
     outdir: './packages/dna-storybook-renderer/dist',
+    packages: 'external',
+});
+
+// dna-storybook-vite
+
+esbuild.build({
+    entryPoints: [
+        './packages/dna-storybook-vite/src/index.ts',
+        './packages/dna-storybook-vite/src/preset.ts',
+    ],
+    target: 'node16',
+    bundle: true,
+    platform: 'node',
+    format: 'cjs',
+    outdir: './packages/dna-storybook-vite/dist',
+    packages: 'external',
+});
+
+esbuild.build({
+    entryPoints: [
+        './packages/dna-storybook-vite/src/index.ts',
+        './packages/dna-storybook-vite/src/preset.ts',
+    ],
+    outExtension: {
+        '.js': '.mjs',
+    },
+    target: 'node16',
+    bundle: true,
+    platform: 'node',
+    format: 'esm',
+    outdir: './packages/dna-storybook-vite/dist',
     packages: 'external',
 });
