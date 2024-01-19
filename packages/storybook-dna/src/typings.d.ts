605a3d9fdc81d495666cd335533c8c34e70e769c
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2024-01-19 14:11:46 +0100

    Fix typings

--- packages/storybook-dna/src/typings.d.ts
+++ packages/storybook-dna/src/typings.d.ts
@@ -4,3 +4,10 @@ declare const STORYBOOK_ENV: 'dna';
 declare const __STORYBOOK_CUSTOM_ELEMENTS_MANIFEST__: Package | undefined;
 declare const __STORYBOOK_CUSTOM_ELEMENTS__: never;
 declare const LOGLEVEL: 'trace' | 'debug' | 'info' | 'warn' | 'error' | 'silent' | undefined;
+
+declare global {
+    interface Window {
+        STORYBOOK_ENV: 'dna';
+        __STORYBOOK_CUSTOM_ELEMENTS_MANIFEST__: Package | undefined;
+    }
+}
